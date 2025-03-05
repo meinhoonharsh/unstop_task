d3ba001cbf990011fb2b4ecf48deff5bc65d9e27
meinhoonharsh <harshprogrammer782@gmail.com>
2025-03-05 17:14:08 +0530

    Updated Room SMM

--- database/migrations/2025_03_05_112825_create_rooms_table.php
+++ database/migrations/2025_03_05_112825_create_rooms_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
+            $table->integer('room_number')->unique();
+            $table->integer('floor');
+            $table->boolean('is_booked')->default(false);
             $table->timestamps();
         });
     }
--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Room;
 
 class RoomSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class RoomSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $totalRooms = 97;
+        $rooms = [];
+        for ($i = 1; $i <= $totalRooms; $i++) {
+            $floor = ceil($i / 10);
+            $rooms[] = [
+                'room_number' => ($floor * 100) + ($i % 10),
+                'floor' => $floor,
+                'is_booked' => false,
+            ];
+        }
+
+        Room::insert($rooms);
     }
 }
